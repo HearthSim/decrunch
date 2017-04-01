b6d5bb6600ef6ca1171c647ff830a6e06bb017be
Robert Nix <mischanix@gmail.com>
2017-03-31 20:25:54 -0500

    Fix OSX build

--- crn_decomp.cpp
+++ crn_decomp.cpp
@@ -1,7 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <string.h>
+#if !defined(__APPLE__)
 #include <malloc.h>
 #ifdef _WIN32
 #define malloc_usable_size _msize
 #endif
+#endif
+
 #include "crn_decomp.h"
