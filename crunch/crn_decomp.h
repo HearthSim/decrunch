585bc75f42c0841da782babae06f2e9bd46f8497
Seven Lju <dna2gle@gmail.com>
2021-09-27 16:24:23 +0800

    make sure macos build works
    - there are no mac check to make sure ptr_bits are 64bit
      but only windows and linux check
    
    ```
    crunch/crn_decomp.h:2353:15: error: cast from pointer to smaller type 'crnd::ptr_bits' (aka 'unsigned int') loses
          information
            if ((uint32) reinterpret_cast<ptr_bits>(p) & (CRND_MIN_ALLOC_ALIGNMENT - 1)) {
                         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    crunch/crn_decomp.h:2378:15: error: cast from pointer to smaller type 'crnd::ptr_bits' (aka 'unsigned int') loses
          information
            if ((uint32) reinterpret_cast<ptr_bits>(p) & (CRND_MIN_ALLOC_ALIGNMENT - 1)) {
                         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    crunch/crn_decomp.h:2390:15: error: cast from pointer to smaller type 'crnd::ptr_bits' (aka 'unsigned int') loses
          information
            if ((uint32) reinterpret_cast<ptr_bits>(p) & (CRND_MIN_ALLOC_ALIGNMENT - 1)) {
    ```

--- crunch/crn_decomp.h
+++ crunch/crn_decomp.h
@@ -403,7 +403,11 @@ typedef uint64 ptr_bits;
 #ifdef __x86_64__
 typedef uint64 ptr_bits;
 #else
+#ifdef __APPLE__
+typedef uint64 ptr_bits;
+#else
 typedef uint32 ptr_bits;
+#endif
 #endif
 #endif
 
